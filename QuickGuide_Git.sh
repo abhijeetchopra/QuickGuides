c9eb4b2d8e5bc3e9441338e047ed0e41d17d0047
Abhijeet Chopra <abhijeetchopra@yahoo.com>
2018-05-30 12:55:00 -0400

    renamed:    QuickGuide_Git.txt -> QuickGuide_Git.sh

---
+++ QuickGuide_Git.sh
@@ -0,0 +1,71 @@
+# --------------------------------------
+# Auth: Abhijeet Chopra
+# Date: Mon Oct 16, 2017
+# Desc: Quick Git Guide
+# --------------------------------------
+
+# GIT COMMANDS
+#---------------------------------------
+
+
+# 2 ways: 1) clone existing repo from server 2) initialize repo locally and push on server
+
+
+# initialize empty git repository
+git init
+
+
+# cloning repo
+mkdir repos
+cd repos
+git clone https://abhijeetchopra@bitbucket.org/tamucinventors/basic-ke.git
+git clone https://github.com/abhijeetchopra/Certifications.git
+
+
+# check status
+git status
+
+# track new files
+git add file_name
+
+# adding all files using wildcards
+git add *
+
+# removing added file 
+git rm file_name
+
+# removing added directory recursively
+git rm -r directory_name
+
+# commiting all files to the stating area / online server
+git commit -m "Adding new files"
+
+# browse changes log
+git log
+
+
+
+# remote (if you did not create a repo online first, then instead of cloning from web, you create local repo and "remote add" it to your server)
+git remote add https://github.com/abhijeetchopra/new-repo/abc.git
+
+# pushing, the -u parameter tell git to remember the parameters so next time we can write just : git push
+git push -u origin master
+
+# pulling
+git pull origin master
+
+# differences, HEAD tell git to check all the differences with the "latest commit"
+git diff HEAD
+
+#staged files are the files that we have told git that are ready to be committed.
+
+# unstage, unstaging files from repo
+git reset new-repo/readme.txt
+
+#Files can be changed back to how they were at the last commit by using the command: git checkout -- <target_file_tobe_undo-ed>
+git checkout -- readme.txt
+
+
+
+# END OF FILE
+# --------------------------------------
